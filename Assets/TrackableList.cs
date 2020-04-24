291459e6b4b94a3b262b602527513a7023f5a210
David Irimia <36772232+Davidenkos@users.noreply.github.com>
2020-04-24 21:37:26 +0300

    First Push

---
+++ Assets/TrackableList.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Vuforia;
+
+public class TrackableList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the Vuforia StateManager
+        StateManager sm = TrackerManager.Instance.GetStateManager();
+
+        // Query the StateManager to retrieve the list of
+        // currently 'active' trackables 
+        //(i.e. the ones currently being tracked by Vuforia)
+        IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours();
+
+        // Iterate through the list of active trackables
+        Debug.Log("List of trackables currently active (tracked): ");
+        foreach (TrackableBehaviour tb in activeTrackables)
+        {
+            print("Trackable: " + tb.TrackableName);
+        }
+    }
+}
---
+++ Assets/sound.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour
+{
+    public AudioClip aClips;
+
+    public AudioSource myAudioSource;
+
+    string btnName;
+
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+        myAudioSource = GetComponent<AudioSource>();
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+            RaycastHit Hit;
+
+            if (Physics.Raycast(ray, out Hit))
+
+            {
+
+                btnName = Hit.transform.name;
+
+                switch (btnName)
+
+                {
+
+                    case "yes":
+
+                        Application.OpenURL("https://www.google.com/");
+
+                        break;
+
+                    case "no":
+
+                        myAudioSource.clip = aClips;
+
+                        myAudioSource.Play();
+                       
+                        break;
+
+                    default:
+
+                        break;
+
+
+
+                }
+
+            }
+
+        }
+
+
+
+    }
+
+}
